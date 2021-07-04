63be18ebb3e7fb64f5f0643dfad7efa4631e6d07
pozm <44528100+pozm@users.noreply.github.com>
2021-07-04 01:48:54 +0100

    inj part

---
+++ Injector/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace Injector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string strExeFilePath = Assembly.GetExecutingAssembly().Location;
+            string dllPath = Directory.GetParent(strExeFilePath) + @"\dll.dll";
+            if (!File.Exists(dllPath))
+            {
+                
+                Console.WriteLine($"Did not find dll. @ {dllPath}");
+                return;
+            
+            }
+            else
+            {
+                // inject
+
+                Assembly asm = Assembly.LoadFrom(dllPath);
+                asm.GetType("dll.Loader")?.GetMethod("InjectLol")?.Invoke(null, new object[0]);
+            }
+        }
+    }
+}
